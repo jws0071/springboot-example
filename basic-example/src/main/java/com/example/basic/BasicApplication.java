aa218ed3b59d133b74ec2e6a82c532c1d3dd028c
jws0071 <jws0071@naver.com>
2019-05-14 16:49:54 +0900

    LOG 진행중

--- basic-example/src/main/java/com/example/basic/BasicApplication.java
+++ basic-example/src/main/java/com/example/basic/BasicApplication.java
@@ -1,13 +1,24 @@
 package com.example.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BasicApplication {
+    private static final Logger logger = LoggerFactory.getLogger(BasicApplication.class);
+
 
     public static void main(String[] args) {
         SpringApplication.run(BasicApplication.class, args);
+        logger.error("error log");
+        logger.warn("warn log");
+        logger.info("info log");
+        logger.debug("debug log");
+        logger.trace("trace log");
+
+
     }
 
 }
